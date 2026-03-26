9a0760018ae625acd35deed117be8b2cf461168e
Florent BOURASSE <fbourasse@jahia.com>
2026-03-26 21:19:57 +0100

    Display of news from https://statuspage.io in the back office
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- src/javascript/init.js
+++ src/javascript/init.js
@@ -1,8 +1,13 @@
 import {registry} from '@jahia/ui-extender';
+import {statuspageIoBanner} from './StatuspageIoBanner/StatuspageIoBanner';
 
 window.jahia.i18n.loadNamespaces('jahia-statuspage-io');
 
 export default function () {
+    registry.add('callback', 'feedbacks', {
+        targets: ['jahiaApp-init:60'],
+        callback: statuspageIoBanner
+    });
 }
 
 console.debug('%c jahia-statuspage-io registered', 'color: #463CBA');
