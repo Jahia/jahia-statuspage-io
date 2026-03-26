e238d68cc68847a54d9422cdbad7cf3475f0961f
Florent BOURASSE <fbourasse@jahia.com>
2026-03-26 20:46:22 +0100

    Initial commit
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

---
+++ src/javascript/init.js
@@ -0,0 +1,8 @@
+import {registry} from '@jahia/ui-extender';
+
+window.jahia.i18n.loadNamespaces('jahia-status-io');
+
+export default function () {
+}
+
+console.debug('%c jahia-status-io registered', 'color: #3c8cba');
